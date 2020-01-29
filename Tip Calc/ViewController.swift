56e1d95f14bcb6e46120fd9635b41c576335928e
David Duong <dduong@csumb.edu>
2020-01-28 17:36:03 -0800

    Initial Commit

---
+++ Tip Calc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calc
+//
+//  Created by David Duong on 1/28/20.
+//  Copyright © 2020 David Duong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
